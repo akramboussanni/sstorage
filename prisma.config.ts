be8ac7c1068e9cc61659db49cddfafe6787545fb
akramboussanni <akramboussanni@gmail.com>
2026-01-22 23:55:52 -0500

    add missing dep

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,6 @@
 export default {
     datasource: {
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL ?? 'file:local.db',
     },
     migrations: {
         seed: process.env.NODE_ENV === 'production' ? 'node prisma/seed.js' : 'npx tsx prisma/seed.ts',
